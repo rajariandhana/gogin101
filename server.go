02ef96034c1b4cb820f3054532fe0070c26fcd65
rajariandhana <rajariandhana@gmail.com>
2024-11-26 13:41:17 +0700

    ep1

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"myapp/controller"
+	"myapp/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+	// server.GET("/test", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "NICE",
+	// 	})
+	// })
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+	server.Run(":8080")
+}
